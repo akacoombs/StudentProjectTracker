4d1305af1fac36cf1a26f06f89fee796e4dae890
akacoombs <timothy_coombs@yahoo.com>
2017-04-14 07:15:18 -0400

    Add files via upload

---
+++ connStud.php
@@ -0,0 +1,12 @@
+<?php
+
+session_start();
+
+function dbconnection(){
+    try{
+        $db = new mysqli("localhost","student","info3410","projecttracker");
+        if($db->connect_errno > 0) return null;
+        return $db;
+    }catch(Exception $e)
+    return null;
+}
